8bd346d1923534b5f8d1dbc53a444505f6d6c265
Christof <no-email@noemail.com>
2016-09-16 08:59:39 +0000

    get quizes from backend

--- src/QuizUI/QuizApp/src/app/quiz-app/quiz-app.component.ts
+++ src/QuizUI/QuizApp/src/app/quiz-app/quiz-app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizProviderService } from '../quiz-provider.service';
+import { Observable } from 'rxjs/Observable';
 import { Quiz } from '../quiz';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-quiz-app',
@@ -9,14 +11,27 @@ import { Quiz } from '../quiz';
   providers: [QuizProviderService]
 })
 export class QuizAppComponent implements OnInit {
+  private _quizes: Quiz[] = null;
 
   constructor(private quizProvider: QuizProviderService) { }
 
   ngOnInit() {
+    this.getquizes();
   }
 
   get quizes(): Quiz[] {
-    return this.quizProvider.GetAvailableQuizes();
+    return this._quizes;
+  }
+
+  get quizesrx(): Observable<Quiz[]> {
+    return this.quizProvider.GetAvailableQuizesRx();
+  }
+
+  getquizes() {
+    this.quizProvider.GetAvailableQuizesRx().subscribe(
+      q => this._quizes = q,
+      e => console.log('error: ' + e)
+    );
   }
 
 }
--- src/QuizUI/QuizApp/src/app/quiz-provider.service.ts
+++ src/QuizUI/QuizApp/src/app/quiz-provider.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@angular/core';
+import 'rxjs/Rx';
+
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
 import { Quiz } from './Quiz';
 
 @Injectable()
 export class QuizProviderService {
 
-  constructor() { }
+  private backendurl = 'http://demoquizapi.azurewebsites.net/api/Quizes';
 
+  constructor(private http: Http) { }
+
+  // OLD:
   GetAvailableQuizes(): Quiz[] {
     return [new Quiz(0, 'The great App Service Quiz'),
             new Quiz(1, 'Some other random Quiz')];
+  }
+
+  GetAvailableQuizesRx(): Observable<Quiz[]> {
+    return this.http.get(this.backendurl)
+                    .do(() => {console.log('observables in action'); })
+                    .map(this.mapToQuiz)
+                    .catch(this.handleError);
+  }
+
+  private mapToQuiz(resp: Response): Quiz[] {
+    console.log('mapping..');
+    let body = resp.json();
+    let quizes = body.map(e => new Quiz(e.Id, e.Name) );
+    return quizes;
+  }
+
+  private handleError(error: any) {
+    return Observable.throw('failed fetching quizes');
   }
 }
