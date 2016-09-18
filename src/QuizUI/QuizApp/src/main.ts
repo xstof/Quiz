0fe4a0250201ddf50b38d410874a9ae711d93688
Christof <no-email@noemail.com>
2016-09-18 15:46:34 +0000

    config service behaves as singleton now

--- src/QuizUI/QuizApp/src/main.ts
+++ src/QuizUI/QuizApp/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+
+// import { ConfigService } from './app/config-service.service' ;
 // import { } from './quiz-app/quiz-app-component' ;
 
 if (environment.production) {
