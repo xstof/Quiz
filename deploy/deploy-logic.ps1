69a505490159159c3c7ad531719a342218842e45
Christof <no-email@noemail.com>
2016-10-04 22:11:24 +0000

    add logic-app template and deployment script

---
+++ deploy/deploy-logic.ps1
@@ -0,0 +1,27 @@
+#
+# deploy ARM infrastructure components
+#
+
+[CmdletBinding()]
+Param(
+  # Params required for App Service Plan creation:
+  [Parameter(Mandatory=$True)]
+  [string]$SubscriptionName,
+  
+  [Parameter(Mandatory=$True)]
+  [string]$RGName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$LogicAppName
+)
+
+Select-AzureRmSubscription -SubscriptionName $SubscriptionName
+Write-Host "Selected subscription: $SubscriptionName"
+
+# Deploy ARM template for quiz-app components (for both API and UI)
+$scriptDir = Split-Path $MyInvocation.MyCommand.Path
+New-AzureRmResourceGroupDeployment -Verbose -Force `
+   -Name "logic" `
+   -ResourceGroupName $RGName `
+   -TemplateFile "$scriptDir/templates/quizapp-logic.json" `
+   -LogicAppName $LogicAppName
